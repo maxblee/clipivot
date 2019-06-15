04e7bcf0bbb9644c7f6cc00bd9a4cb186b143297
Max Lee <maxbmhlee@gmail.com>
2019-06-15 16:01:59 -0400

    added basic structure

--- src/aggregation/aggfunc.rs
+++ src/aggregation/aggfunc.rs
@@ -2,3 +2,25 @@
 //! I've heavily based its design off of [this useful](http://is.gd/N0N5ni)
 //! playground that I found on
 //! [Reddit](https://www.reddit.com/r/rust/comments/2rdoxx/enum_variants_as_types/).
+
+#![feature(associated_types)]
+
+enum AggType {
+    Count,
+    UniqueCount,
+}
+
+#[derive(Debug, Show, PartialEq)]
+struct CountRecords;
+
+trait AggregationMethod {
+    type Aggfunc;
+}
+
+impl AggregationMethod for Count {
+    type Aggfunc = Count;
+}
+
+impl AggregationMethod for UniqueCount {
+    type Aggfunc = UniqueCount;
+}
