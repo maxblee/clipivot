921a0386b29e00e3dd1d523215af17a0773863df
Max Lee <maxbmhlee@gmail.com>
2019-06-18 08:36:49 -0400

    updated structure with methods for Count

--- src/aggregation/aggfunc.rs
+++ src/aggregation/aggfunc.rs
@@ -7,6 +7,12 @@ pub enum AggTypes {
 pub trait AggregationMethod {
     type Aggfunc;
 
+    /// Returns the Aggregation method (e.g. AggTypes::Count)
+    fn get_aggtype(&self) -> AggTypes;
+    /// Instantiates a new Aggregation method
+    fn new(parsed_val: String) -> Self;
+    /// Updates an existing method
+    fn update(&mut self, parsed_val: String);
 }
 
 struct Count {
@@ -15,6 +21,14 @@ struct Count {
 
 impl AggregationMethod for Count {
     type Aggfunc = Count;
+
+    fn get_aggtype(&self) -> AggTypes { AggTypes::Count }
+    fn new(parsed_val: String) -> Self {
+        Count { val: 1 }
+    }
+    fn update(&mut self, parsed_val: String) {
+        self.val += 1;
+    }
 }
 
 pub struct Aggregator<T>
