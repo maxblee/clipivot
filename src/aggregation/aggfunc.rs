2365293178efb91f5e7c106f0df4da11da9c2527
Max Lee <maxbmhlee@gmail.com>
2019-06-18 09:11:41 -0400

    new record should work

--- src/aggregation/aggfunc.rs
+++ src/aggregation/aggfunc.rs
@@ -10,9 +10,9 @@ pub trait AggregationMethod {
     /// Returns the Aggregation method (e.g. AggTypes::Count)
     fn get_aggtype(&self) -> AggTypes;
     /// Instantiates a new Aggregation method
-    fn new(parsed_val: String) -> Self;
+    fn new(parsed_val: &str) -> Self;
     /// Updates an existing method
-    fn update(&mut self, parsed_val: String);
+    fn update(&mut self, parsed_val: &str);
 }
 
 struct Count {
@@ -23,10 +23,10 @@ impl AggregationMethod for Count {
     type Aggfunc = Count;
 
     fn get_aggtype(&self) -> AggTypes { AggTypes::Count }
-    fn new(parsed_val: String) -> Self {
+    fn new(parsed_val: &str) -> Self {
         Count { val: 1 }
     }
-    fn update(&mut self, parsed_val: String) {
+    fn update(&mut self, parsed_val: &str) {
         self.val += 1;
     }
 }
@@ -36,4 +36,14 @@ where
     T: AggregationMethod,
 {
     aggregations: HashMap<(String, String), T>,
+}
+
+impl <T: AggregationMethod> Aggregator<T> {
+    fn update_aggregations(&mut self, indexname: String, columnname: String, parsed_val: &str) {
+        // modified from
+        // https://users.rust-lang.org/t/efficient-string-hashmaps-for-a-frequency-count/7752
+        self.aggregations.entry((indexname, columnname))
+            .and_modify(|val| val.update(parsed_val))
+            .or_insert(T::new(parsed_val));
+    }
 }
