c216e8e001d66d6f392a3896dd2738e8523820a3
Max Lee <maxbmhlee@gmail.com>
2019-06-30 14:18:32 -0400

    added datetime conversion test that currently fails

--- src/parsing.rs
+++ src/parsing.rs
@@ -10,6 +10,7 @@
 //! can automatically determine the type of record appearing in the values column.
 use std::str::FromStr;
 use rust_decimal::Decimal;
+use chrono::{DateTime, NaiveDateTime, TimeZone, Utc};
 use crate::errors::CsvPivotError;
 
 /// The types of data `csvpivot` converts `&str` records into.
@@ -23,6 +24,8 @@ pub enum ParsingType {
     Numeric(Option<Decimal>),
     /// This is used for numeric operations involving minimum and maximum, as well as standard deviation
     FloatingPoint(Option<f64>),
+    /// This is used for parsing date types
+    DateTypes(Option<chrono::DateTime<Utc>>),
 }
 
 /// Stores information about the type of data appearing in the values column
@@ -38,6 +41,7 @@ pub struct ParsingHelper {
     parse_empty: bool,
 }
 
+// TODO: Get rid of this
 impl Default for ParsingHelper {
     fn default() -> ParsingHelper {
         ParsingHelper {
@@ -56,9 +60,11 @@ impl ParsingHelper {
             parse_empty: true,
         }
     }
+
     /// Converts a new `&str` value read in from a CSV file into a format determined
     /// by the `values_type` attribute of `ParsingHelper`. Raises an error
     /// if the value read in is not compatible with the parsing type.
+    // TODO: Get rid of this
     pub fn set_numeric() -> ParsingHelper {
         ParsingHelper {
             values_type: ParsingType::Numeric(None),
@@ -67,6 +73,7 @@ impl ParsingHelper {
         }
     }
 
+    // TODO: Get rid of this
     pub fn set_floating() -> ParsingHelper {
         ParsingHelper {
             values_type: ParsingType::FloatingPoint(None),
@@ -91,8 +98,13 @@ impl ParsingHelper {
             ParsingType::Text(_) => Ok(ParsingType::Text(Some(new_val.to_string()))),
             ParsingType::Numeric(_) => ParsingHelper::parse_numeric(new_val),
             ParsingType::FloatingPoint(_) => ParsingHelper::parse_floating(new_val),
+            ParsingType::DateTypes(_) => ParsingHelper::parse_datetime(new_val),
         }?;
         Ok(Some(parsed_val))
+    }
+
+    fn parse_datetime(new_val: &str) -> Result<ParsingType, CsvPivotError> {
+        Ok(ParsingType::DateTypes(None))
     }
 
     fn parse_numeric(new_val: &str) -> Result<ParsingType, CsvPivotError> {
@@ -103,5 +115,35 @@ impl ParsingHelper {
     fn parse_floating(new_val: &str) -> Result<ParsingType, CsvPivotError> {
         let num : f64 = new_val.parse().or(Err(CsvPivotError::ParsingError))?;
         Ok(ParsingType::FloatingPoint(Some(num)))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::parsing::ParsingType::DateTypes;
+
+    #[test]
+    fn test_automatic_date_conversion() -> Result<(), CsvPivotError> {
+        // determines whether valid month, day, year formats get properly handled
+        // Note that this should also handle ISO8601 formats
+        let parsable_dates = vec!["2003-01-03", "1/3/03", "January 3, 2003",
+        "Jan 3, 2003", "3 Jan 2003", "3 January 2003", "Jan 3, 2003 12:00 a.m.",
+        "Jan 3, 2003 12:00:00 a.m.", "Jan 3, 2003 00:00", "Jan 3, 2003 00:00:00",
+        "2003-01-03T00:00:00", "2003-01-03T00:00:00+00:00", "2003.01.03", "Jan. 3, 2003"];
+        let helper = ParsingHelper::from_parsing_type(DateTypes(None));
+        for date in parsable_dates {
+            let parsed_opt_date = helper.parse_val(date)?;
+            let parsed_date = match parsed_opt_date {
+                Some(ParsingType::DateTypes(Some(val))) => Ok(val),
+                _ => Err(CsvPivotError::ParsingError)
+            }?;
+            let expected_utc = DateTime::<Utc>::from_utc(chrono::Utc
+                .ymd(2003, 1, 3)
+                .and_hms(0,0,0)
+                .naive_utc(), Utc);
+            assert_eq!(parsed_date, expected_utc);
+        }
+        Ok(())
     }
 }
